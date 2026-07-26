d1dbeec7430695d28ca02bef10a41a71cd3d08e5
Nahid-2024331523 <bashirshahriernahid@gmail.com>
2026-07-26 22:24:39 +0600

    more about doubly link list

---
+++ Link list/Doubly Linked List/Delete_at_any_posiion.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void print_forward(Node* head)
+{
+    Node* tmp=head;
+    while(tmp!=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    cout<<endl;
+}
+void print_backward(Node* tail)
+{
+    Node* tmp=tail;
+    while(tmp!=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->prev;
+    }
+    cout<<endl;
+}
+void delete_any(Node* &head , int idx)
+{
+    Node* tmp=head;
+    for(int i=1 ; i<idx ; i++){
+        tmp=tmp->next;
+    }
+    Node* deletenode=tmp->next;
+    tmp->next=tmp->next->next;
+    tmp->next->prev=tmp;
+    delete deletenode;
+}
+int main()
+{
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(30);
+    Node* c=new Node(40);
+    Node* tail=new Node(50);
+    head->next=a;
+    a->prev=head;
+    a->next=b;
+    b->prev=a;
+    b->next=c;
+    c->prev=b;
+    c->next=tail;
+    tail->prev=c;
+    delete_any(head,3);
+    print_forward(head);
+    print_backward(tail);
+    return 0;
+}
